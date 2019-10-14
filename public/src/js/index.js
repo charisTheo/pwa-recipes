61d4d9e2ecc026a1e2b8476238f091d471c6be73
charisTheo <dev@charistheo.io>
2019-10-14 17:13:32 +0100

    Implement adding an item to shopping cart and toggling it on button click. Update number of cart items when adding items to the shopping cart.

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -5,18 +5,57 @@ if (!global._babelPolyfill) {
 import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-toolbar/paper-toolbar.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-item/paper-item-body.js';
+import '@polymer/paper-item/paper-icon-item.js';
 
 const VAPID_PUBLIC_KEY = 'BCvnBFnsPt6MPzwX_LOgKqVFG5ToFJ5Yl0qDfwrT-_lqG0PqgwhFijMq_E-vgkkLli7RWHZCYxANy_l0oxz0Nzs';
+const NOTIFICATIONS_ACTIVE_URL = 'img/notifications-active.svg';
+const NOTIFICATIONS_NONE_URL = 'img/notifications-none.svg';
 const snackBar = document.getElementById('snackbar');
+const notificationsRequestButton = document.getElementById('notifications-request-button');
+const shoppingCartButton = document.getElementById('shopping-cart-button');
+const numberOfCartItemsEl = document.getElementById('number-of-cart-items');
+const cartItemsContainer = document.getElementById('cart-items-container');
+const cartCloseButton = document.getElementById('cart-close-button');
 
-window.addEventListener('load', () => {
-    // if ('serviceWorker' in navigator) {
-    //     navigator.serviceWorker.register('./service-worker.js');
-    // }
+var db;
+
+window.addEventListener('load', async () => {
+    // TODO register service worker
+    await registerServiceWorker();
+
+    // TODO handle answers from the push notification action clicks 
+    // checkPushNotificationActions();
+
+    configureLocalDatabase();
+
+    const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
+    addToCartButtons.forEach(button => button.addEventListener('click', addToCart));
+
+    shoppingCartButton.addEventListener('click', toggleShoppingCart);
+    
+    cartCloseButton.addEventListener('click', toggleShoppingCart);
+
+    // initialise notificationsRequestBtn notification icon src after checking the pushManager's permission
+    const pushPermission = await getNotificationPermission();
+    const notificationButtonIconSrc = pushPermission === "granted" ? NOTIFICATIONS_ACTIVE_URL : NOTIFICATIONS_NONE_URL;
+    notificationsRequestButton.setAttribute('src', notificationButtonIconSrc);
+
+    notificationsRequestButton.addEventListener('click', requestNotificationPermission);
+});
+
+const registerServiceWorker = () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./service-worker.js');
+    }
+}
+
+const checkPushNotificationActions = () => {
     const queryString = document.location.search;
-
     if (!!queryString) {
-        // TODO
         // ? get push-notifications-are-cool parameter from the URL bar
         const pushNotificationsAreCool = getValueFromUrlQueryString(queryString);
 
@@ -30,44 +69,109 @@ window.addEventListener('load', () => {
             window.history.replaceState({}, document.title, '/');
         }, 2500);
     }
-});
+}
 
-window.wipeData = async () => {
-    // * Removes user data from the database
-    //  * Also removes the httpOnly cookie 
-    const response = await fetch(`/user/remove`, { method: 'GET' });
-    if (response.status === 400) {
-        showSnackBar("There was an error while deleting your data 😕. Please Try again.");
-        return;
-    } 
+const configureLocalDatabase = () => {
+    let DBOpenRequest = window.indexedDB.open("shoppingCart", 1);
+
+    DBOpenRequest.onerror = function(event) {
+        showSnackBar('Error loading database.');
+    };
+
+    DBOpenRequest.onsuccess = function(event) {
+        showSnackBar('Database initialised.');
+
+        db = DBOpenRequest.result;
+        initialiseNumberOfCartItems();
+    };
+
+    DBOpenRequest.onupgradeneeded = function(event) {
+        let db = event.target.result;
+        db.onerror = function(event) {
+            console.warn("DBOpenRequest.onupgradeneeded: db.onerror -> event", event);
+            showSnackBar('Error loading database.');          
+        };
     
-    // * wipe cache
-    caches.keys()
-    .then(cacheNames => {
-        cacheNames.map(cache => caches.delete(cache));
-    });
+        let objectStore = db.createObjectStore("shoppingCart", { keyPath: "name" });
+        objectStore.createIndex("name", "name", { unique: false });
+        objectStore.createIndex("price", "price", { unique: false });
+        objectStore.createIndex("image", "image", { unique: false });
+    };
+}
 
-    // * unregister all service workers
-    const registrations = await navigator.serviceWorker.getRegistrations();
-    const registrationsPromises = registrations.map(registration => {
-        registration.unregister();
-    });
+const addToCart = (e) => {
+    const item = JSON.parse(e.target.getAttribute('data-item'));
+    const { name, price, image } = item;
 
-    await Promise.all(registrationsPromises);
+    const transaction = db.transaction(["shoppingCart"], "readwrite");
+    // transaction.onerror = function(error) {
+    //     showSnackBar(`There was an error with the database while adding ${name} to your cart 😕`);
+    // };
+    
+    const objectStore = transaction.objectStore('shoppingCart');
+    const objectStoreRequest = objectStore.add(item);
+    objectStoreRequest.onsuccess = function() {
+        const countRequest = objectStore.count();
+        countRequest.onsuccess = () => {
+            // * Update the number of cart items
+            updateNumberOfCartItems(countRequest.result);
+        }
+        addItemDescriptionToShoppingCart(item);
+        showSnackBar(`${name} has been added to your cart! 🎊🛒`);
+    }
+    objectStoreRequest.onerror = function(error) {
+        const itemExistsInCart = error.target.error.message === 'Key already exists in the object store.';
+        // TODO increment item in shopping cart else show the error in snack bar
+        showSnackBar(`There was an error while adding ${name} to your cart 😕`);
+    }
+}
 
-    // ! Cannot revoke browser notification permissions yet. 
-    // ! https://stackoverflow.com/questions/28478185/remove-html5-notification-permissions/
+const initialiseNumberOfCartItems = async () => {
+    const transaction = db.transaction(["shoppingCart"], "readwrite");
+    const objectStore = transaction.objectStore('shoppingCart');
+    const getAllItemsRequest = objectStore.getAll();
+    const countRequest = objectStore.count();
+    countRequest.onsuccess = () => {
+        // * Update the number of cart items
+        updateNumberOfCartItems(countRequest.result);
+    };
+    getAllItemsRequest.onsuccess = () => {
+        const cartItems = getAllItemsRequest.result;
+        cartItems.map(cartItem => {
+            addItemDescriptionToShoppingCart(cartItem);
+        });
+    };
+}
 
-    showSnackBar("All your data has been deleted!");
+const updateNumberOfCartItems = (numberOfCartItems) => {
+    if (numberOfCartItems === 0) {
+        numberOfCartItemsEl.style.visibility = 'hidden';
+    } else {
+        numberOfCartItemsEl.style.visibility = 'visible';
+        numberOfCartItemsEl.innerText = numberOfCartItems;
+    }
+}
+
+// Possible values: 'prompt', 'denied', or 'granted'
+const getNotificationPermission = async () => {
+    const registration = await navigator.serviceWorker.getRegistration('/');
+    const permission = await registration.pushManager.permissionState({userVisibleOnly: true});
+    return permission;
 }
 
 // ! ask for permission only when the user clicks
-window.requestNotificationPermission = () => {
+const requestNotificationPermission = () => {
     navigator.serviceWorker.getRegistration('/').then(registration => {
         registration.pushManager.permissionState({userVisibleOnly: true}).then(permission => {
             // Possible values are 'prompt', 'denied', or 'granted'
             if (permission === "prompt" || permission === "granted") {
-                subscribeToPushManager(registration);
+                subscribeToPushManager(registration).then(() => {
+                    notificationsRequestButton.src = NOTIFICATIONS_ACTIVE_URL;
+                }).catch(() => {
+                    notificationsRequestButton.src = NOTIFICATIONS_NONE_URL;
+                });
+            } else {
+                showSnackBar("🚫 You have blocked notifications for this site!");
             }
         });
     });
@@ -120,7 +224,7 @@ const subscribeToPushManager = async registration => {
 
     if (!subscription) {
         showSnackBar("Couldn't subscribe to the Push Manager!");
-        return;
+        return Promise.reject();
     }
     console.log('Sending subscription to the API...');
 
@@ -157,6 +261,23 @@ const showSnackBar = message => {
         snackBar.classList.remove('show');
         snackBar.innerText = '';
     }, 5000);
+}
+
+const addItemDescriptionToShoppingCart = item => {
+    const html = ` <paper-icon-item id="shopping-cart-item-${item.name}">
+        <div class="avatar" style="background-image: url(./img/products/${item.image})" slot="item-icon"></div>
+        <paper-item-body two-line>
+            <div>${item.name}</div>
+            <div secondary>${item.price}</div>
+        </paper-item-body>
+        <paper-icon-button icon="delete" alt="remove item from shopping cart"></paper-icon-button>
+    </paper-icon-item>`;
+
+    document.getElementById('cart-items').innerHTML += html;
+}
+
+const toggleShoppingCart = (event) => {
+    cartItemsContainer.classList.toggle('show');
 }
 
 const getValueFromUrlQueryString = queryString => {
