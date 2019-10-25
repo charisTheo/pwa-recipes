63492bb0d381e9d8ca2fa05d0a6175ee4e79d34e
charisTheo <dev@charistheo.io>
2019-10-25 16:28:42 +0100

    Add localhost to the allowed origins

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(publicDir));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://www.charistheo.io");
+    res.header("Access-Control-Allow-Origin", "https://www.charistheo.io, http://localhost:9000");
     next();
 });
 
