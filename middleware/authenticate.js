35cf2c667cca23bacc051d137b4aa2f0da73671c
charisTheo <dev@charistheo.io>
2019-10-28 16:42:52 +0000

    Added cookie parser and logs for authentication

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 const { User } = require('../models/user');
 
 const authenticate = async (req, res, next) => {
-    const { _id } = req.cookies;
+    const { _id } = req.cookies || res.signedCookies;
+    console.log("TCL: authenticate -> req.cookies", req.cookies);
+    console.log("authenticate -> res.signedCookies", res.signedCookies);
     const user = await User.findOne({ _id });
 
     if (!user) {
@@ -11,11 +13,13 @@ const authenticate = async (req, res, next) => {
         console.log('authenticate: create a new one and send an http-only cookie');
         const newUserId = mongoose.Types.ObjectId();
         console.log("authenticate: newUserId", newUserId);
+
         const newUser = new User({_id: newUserId});
         await newUser.save();
         
+        res.cookie('_id', newUserId.toHexString(), { expires: new Date(Date.now() + 900000), httpOnly: true, domain: 'charistheo.io' });
         req.user = newUser;
-        res.setHeader('Set-Cookie', [`_id=${newUserId.toHexString()}; HttpOnly`]);
+        // res.setHeader('Set-Cookie', [`_id=${newUserId.toHexString()}; HttpOnly`]);
         next();
 
     } else {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const path = require('path');
 const express = require('express');
+const session = require('cookie-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -14,13 +15,17 @@ const PORT = process.env.PORT || 3000;
 const publicDir = path.join(__dirname + `/public`);
 
 app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(publicDir));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type");
     next();
 });
+app.use(session({
+    name: 'session',
+    secret: process.env.COOKIE_SECRET
+}))
 
 routes(app);
 
