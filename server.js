a3ea6fe286e4a647c669c7d3cbcd028bd73a2553
charisTheo <dev@charistheo.io>
2019-10-28 17:19:13 +0000

    Fixed http headers.

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(publicDir));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "charistheo.io");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type");
     next();
 });
