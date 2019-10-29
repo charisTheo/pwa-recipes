2cdf9fa63c50535fe96ac7f4ffda5476dbf8948b
charisTheo <dev@charistheo.io>
2019-10-29 11:54:50 +0000

    Addded cookie session

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('./config/config');
 
 const path = require('path');
 const express = require('express');
-// const session = require('cookie-session');
+const session = require('cookie-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -15,8 +15,15 @@ const PORT = process.env.PORT || 3000;
 const publicDir = path.join(__dirname + `/public`);
 
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(express.static(publicDir));
+app.use(cookieParser());
+app.use(session({
+    cookieName: '_id',
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {httpOnly: true, secure: true}
+}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://www.charistheo.io");
     res.header("Access-Control-Allow-Credentials", true);
