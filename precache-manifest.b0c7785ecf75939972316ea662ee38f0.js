ac0181da6980866d1b8c0809ea39062cec230b1d
charisTheo <dev@charistheo.io>
2019-12-11 13:59:13 +0000

    Updates

---
+++ precache-manifest.b0c7785ecf75939972316ea662ee38f0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f341a5e74e17038123",
+    "url": "2.js"
+  },
+  {
+    "revision": "e938b619af609fac057d",
+    "url": "3.js"
+  },
+  {
+    "revision": "c7eaade1591d8cfc3076",
+    "url": "4.js"
+  },
+  {
+    "revision": "704084fc84661416dad9",
+    "url": "5.js"
+  },
+  {
+    "revision": "edcb3ead103008ab8a0b",
+    "url": "6.js"
+  },
+  {
+    "revision": "1ca5997b2cc7ce31e2859f128d8a050d",
+    "url": "bower_components/webcomponentsjs/gulpfile.js"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "bower_components/webcomponentsjs/webcomponents-loader.js"
+  },
+  {
+    "revision": "b9128d2b62f7045360553516f9a3b673",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "9b3752dd15ec9c329721d88129e6b5b4",
+    "url": "favicon/android-chrome-192x192.png"
+  },
+  {
+    "revision": "7d86966a35dad455379c33a979d84c9c",
+    "url": "favicon/android-chrome-512x512.png"
+  },
+  {
+    "revision": "6fb5595f7cfad8c3a71a85927cd50765",
+    "url": "favicon/apple-touch-icon.png"
+  },
+  {
+    "revision": "874ad29cc6533140651e56d2badd23c7",
+    "url": "favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "5d6b4edc9fd4209604cf1fb4bc73b4e5",
+    "url": "favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "b9128d2b62f7045360553516f9a3b673",
+    "url": "favicon/favicon.ico"
+  },
+  {
+    "revision": "908ee02a97acb835ed36d5b37d7564bc",
+    "url": "favicon/maskable_icon.png"
+  },
+  {
+    "revision": "4809e7daa7602f49e4ed",
+    "url": "home.js"
+  },
+  {
+    "revision": "42865eb90c8c5d904b4b30d8da806bf1",
+    "url": "img/bin.png"
+  },
+  {
+    "revision": "0b14c98f83e8eac028f6d77adb68b486",
+    "url": "img/cart-abandon.png"
+  },
+  {
+    "revision": "e503834e8032fa06845c83d1bfee3fb8",
+    "url": "img/cool.png"
+  },
+  {
+    "revision": "e4aa6274b86adf1c42b288e5b9008553",
+    "url": "img/not-cool.png"
+  },
+  {
+    "revision": "68d07494c05d1f8fed0614b4f4c419ae",
+    "url": "img/offline-requests.png"
+  },
+  {
+    "revision": "1d9a793c6dc1692e45ec527f92fdb646",
+    "url": "img/placeholder-image.png"
+  },
+  {
+    "revision": "1b3d24180e63fde28a10fb1878707578",
+    "url": "img/planets.svg"
+  },
+  {
+    "revision": "85b0544b16b9d11c924be85f6da54821",
+    "url": "img/push-examples.png"
+  },
+  {
+    "revision": "6d6119fda62054c1dac7fa957a0014a8",
+    "url": "img/pwa-logo.png"
+  },
+  {
+    "revision": "c450453e2e1948cfb64b9ac801cd786b",
+    "url": "img/share-24px.svg"
+  },
+  {
+    "revision": "053d74b1a2eae76aab9199a6309b4cdb",
+    "url": "index.html"
+  },
+  {
+    "revision": "58cdc47cb536fcdf48eb5be056cb48f8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "5c4f79598bc1fc3c5f65d2664b23cee9",
+    "url": "offline.html"
+  },
+  {
+    "revision": "878dd367e52ff56b4aa0",
+    "url": "tabs.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-importScripts("precache-manifest.870d42406272213b6d5be017dabe759a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.b0c7785ecf75939972316ea662ee38f0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox
-// ? Both images are precached in the __precacheManifest file
 const OFFLINE_PAGE_URL = '/offline.html';
 const PLACEHOLDER_IMAGE_URL = '/img/placeholder-image.png';
 const PAGE_ICON_URL = '/favicon/android-chrome-192x192.png';
@@ -18,6 +17,7 @@ addEventListener('activate', event => {
 
 addEventListener('message', event => {
   if (event.data && event.data.type === 'NEW_VERSION') {
+    // console.warn("TCL: message = NEW_VERSION | event", event);
     skipWaiting();
   }
 });
@@ -47,8 +47,7 @@ addEventListener('fetch', async event => {
     }
 });
 
-self.__precacheManifest = (self.__precacheManifest || []).concat([PLACEHOLDER_IMAGE_URL, PAGE_ICON_URL, OFFLINE_PAGE_URL]);
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+workbox.precaching.precacheAndRoute((self.__precacheManifest || []).concat([PLACEHOLDER_IMAGE_URL, PAGE_ICON_URL, OFFLINE_PAGE_URL]));
 
 workbox.routing.registerRoute(
   /(https:\/\/fonts\.(googleapis|gstatic)\.com)/,
